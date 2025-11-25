fd9729fb59e4fe63832b9d637c07c82e31dfde91
justin-win <justinnanguyen@gmail.com>
2025-11-25 02:31:53 -0600

    container implementation

---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"syscall"
+)
+
+func main() {
+	if argc := len(os.Args); argc < 2 {
+		panic("./main <run/arg> <commands>")
+	}
+	switch os.Args[1] {
+	case "run":
+		parent()
+	case "child":
+		child()
+	default:
+		panic("./main <run/arg> <commands>")
+	}
+}
+
+func parent() {
+	// Create a clone of our program
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	// This clone should have a separate namespace for PID, MNT, and network for isolatio
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
+		Unshareflags: syscall.CLONE_NEWNS, // CRITICAL: Unshare the mount namespace so it doesn't share with parent
+												// This is so when we unmount our running FS, it won't impact the parent/host FS
+	}
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+}
+
+func child() {
+	root := "/home/justin/projects/containers-go/rootfs"
+	syscall.Sethostname([]byte("container"))
+
+	// Mount our desired location into a file system for our child
+	must(syscall.Mount(root, root, "", syscall.MS_BIND, ""))
+
+	// Create a directory that will hold our current running file system
+	must(os.MkdirAll(root + "/oldrootfs", 0707))
+
+	// Our current running FS (host "/") becomes root (arg1)
+	// Our old FS (host "/") is put into <root>/oldrootfs
+		// This is better than chroot since there is no escape
+		// Whereas pivot_root allows us to set the root of our FS to our current namespace
+	must(syscall.PivotRoot(root, root + "/oldrootfs"))
+
+	// Change into our current running FS
+	must(os.Chdir("/"))
+
+	// Mount proc to be able to view processes
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
+	// Unmount our host FS (host "/") from our container
+		// Because we've unshared(CLONE_NEWNS), it won't unmount from our host OS
+	must(syscall.Unmount("oldrootfs", syscall.MNT_DETACH))
+	must(os.Remove("oldrootfs"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	must(cmd.Run())
+
+	// Cleanup
+	must(syscall.Unmount("proc", syscall.MNT_DETACH))
+}
+
+func must(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
